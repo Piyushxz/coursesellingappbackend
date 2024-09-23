427fbfeb1ccd3df0ffd054c7ce993a1c4f795c10
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-09-23 23:33:22 +0530

    added course routes

--- routes/course.js
+++ routes/course.js
@@ -1,4 +1,6 @@
 const { Router } = require("express")
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel, courseModel } = require("../schema/db");
 
 
 
@@ -7,17 +9,31 @@ const courseRouter = Router()
 
 
 
-courseRouter.post("/purchase",(req,res)=>{
-    res.json({
-        message:"Purchase"
-    })
+courseRouter.post("/purchase",userMiddleware,async (req,res)=>{
+    const userId = req.userId;
+
+    const courseId = req.body.courseId;
+
+    try{
+        await purchaseModel.create({
+            userId:userId,
+            courseId:courseId
+        })
+    }catch(e){
+        res.status(403).json({message:"Could not purchase"})
+    }
+
 })
 
 
-courseRouter.post("/preview",(req,res)=>{
-    res.json({
-        message:"preview"
-    })
+courseRouter.post("/preview",async(req,res)=>{
+    try{
+        const courses = await courseModel.find({})
+
+        res.json(200).json({courses})
+    }catch(e){
+        res.status(403).json({message:"Could not fetch courses"})
+    }
 })
 
 module.exports = {
